95d50c043a1f2c3c2bf42ee42f578d0e44372084
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-08-05 16:01:02 +0100

    API integrated

--- src/Components/SearchEngine.js
+++ src/Components/SearchEngine.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 
 function SearchEngine() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse() {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
